7d6c16f3f13a571b29f06ba1fa035e012b319181
Fabien Marceau <fabien.marceau@gmail.com>
2023-02-20 23:13:02 +0100

    axios requete

--- Vue/src/views/Products.vue
+++ Vue/src/views/Products.vue
@@ -1,10 +1,44 @@
 <template>
-    <h1>Products</h1>
+    <div class="main">
+  <h1>Products</h1>
+  <div :key="index" v-for="(nom, index) in nom">
+    <div class="names">{{ nom.title }}</div>
+  </div>
+  </div>
 </template>
 
-<script></script>
-<style>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      nom: null,
+    };
+  },
+  mounted() {
+    axios.get("https://fakestoreapi.com/products").then((reponse) => {
+      this.nom = reponse.data;
+      console.log(this.nom);
+    });
+  },
+};
+</script>
 
-h1{
-    font-size: 43px;
-}</style>
+<style>
+.main{
+    background-color: rgb(255, 255, 255);
+    height: 100%;
+}
+h1 {
+  font-size: 16px;
+}
+
+.names {
+  height: 100px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 30px;
+}
+</style>
